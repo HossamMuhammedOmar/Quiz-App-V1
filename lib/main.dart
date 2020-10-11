2c0b508223d2ec37a2b42844a5d0ca421db012f3
HossamMuhammedOmar <hossamomar9876@gmail.com>
2020-10-11 15:59:46 +0200

    update project

--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,31 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int quistionIdx = 0;
+
+  var quistions = [
+    'What\'s your name ?',
+    'How old are you?',
+    'What\'s your favorite animale?'
+  ];
+
+  void answerQuistion() {
+    if (quistionIdx < quistions.length - 1) {
+      quistionIdx += 1;
+      print(quistionIdx);
+    }
+  }
+
+// 010 0974 2376
+// 02 37939045
   @override
   Widget build(BuildContext context) {
-    var quistions = [
-      'What\'s your name ?',
-      'How old are you?',
-      'What\'s your favorite animale?'
-    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -19,18 +36,36 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: [
-            Text('Quistions'),
+            Text(quistions[quistionIdx]),
             RaisedButton(
               child: Text('Answer 1'),
-              onPressed: () => print('Answer 1'),
+              onPressed: () {
+                setState(
+                  () {
+                    answerQuistion();
+                  },
+                );
+              },
             ),
             RaisedButton(
               child: Text('Answer 2'),
-              onPressed: () => print('Answer 2'),
+              onPressed: () {
+                setState(
+                  () {
+                    answerQuistion();
+                  },
+                );
+              },
             ),
             RaisedButton(
               child: Text('Answer 3'),
-              onPressed: () => print('Answer 3'),
+              onPressed: () {
+                setState(
+                  () {
+                    answerQuistion();
+                  },
+                );
+              },
             ),
           ],
         ),
